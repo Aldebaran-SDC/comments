171bed32554c239f3f691f8acb4fc92fef953e5a
Takanori Sono <takanori.sono@gmail.com>
2020-03-24 18:52:24 -0700

    Recommit postgreSQL schema

--- database/pgSQL/schema.js
+++ database/pgSQL/schema.js
@@ -24,17 +24,6 @@ var createTable =
   posted_at       date
 )`;
 
-// var createTable = 
-// `CREATE TABLE comments2 (
-//   user_name       character varying,
-//   user_id         integer,
-//   user_icon       character varying,
-//   song_id         integer,
-//   message         character varying,
-//   audio_position  integer,
-//   posted_at       date
-// )`;
-
 pool.query(createTable,(err, result) => {
   if (err) {console.log(err)}
   console.log(result);
